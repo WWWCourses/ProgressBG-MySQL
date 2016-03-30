03647028513a03dd565e5b0f787de1fc0a6ba3cd
WWWCourses <ivawebcourses@gmail.com>
2016-03-30 21:00:14 +0300

    excercies

---
+++ pages/themes/beginners/Insert_delete_update_data/books_db.sql
@@ -0,0 +1,21 @@
+0. CONNECT TO MySQL as ROOT
+1. CRETE DB: books_db
+2. CREATE books_db_admin;
+3. GRANT ALL PRIVILEGES ON ...
+4. CONNECT to books_db as books_db_admin
+4. CREATE TABLE :
+
+table: book
+----------------
+id (primary key, auto_increment)
+name,
+author,
+year;
+
+table: author
+----------------
+id,
+book
+fname,
+lname,
+
